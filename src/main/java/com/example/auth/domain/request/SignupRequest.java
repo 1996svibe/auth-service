ae1b7b46754811a53791eb35abe9047bd188c28f
1996svibe <dndwlscjwo87@gmail.com>
2023-08-31 15:13:04 +0900

    auth

---
+++ src/main/java/com/example/auth/domain/request/SignupRequest.java
@@ -0,0 +1,20 @@
+package com.example.auth.domain.request;
+
+import com.example.auth.domain.entity.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupRequest {
+    private String name;
+    private String number;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
